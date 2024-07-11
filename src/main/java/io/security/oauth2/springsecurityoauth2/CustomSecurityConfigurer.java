07ba9a9aa62c66cb464c2a47b01d61056d777d26
nyoongoon <nyoongoon@naver.com>
2024-07-12 06:33:22 +0900

    초기화과정이해 SecurityBuilder / SecurityConfigurer

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomSecurityConfigurer.java
@@ -0,0 +1,32 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+public class CustomSecurityConfigurer extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
+
+    private boolean isSecure;
+
+    @Override
+    public void init(HttpSecurity builder) throws Exception {
+        super.init(builder);
+        System.out.println("CustomSecurityConfigurer.init");
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        super.configure(builder);
+        System.out.println("CustomSecurityConfigurer.configure");
+        if(isSecure){
+            System.out.println("https is required");
+        }else{
+            System.out.println("https is optional");
+        }
+    }
+
+    // 체이닝 메서드를 위한 this 반환
+    public CustomSecurityConfigurer setFlag(boolean isSecure){
+        this.isSecure = isSecure;
+        return this;
+    }
+}
---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,23 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    /**
+     * 시큐리티 필터체인 타입의 빈 생성하기
+     * HttpSecurity.build()하여 SecurityFilterChain를 생성할 수 있다.
+     */
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.authorizeRequests().anyRequest().authenticated();
+            httpSecurity.formLogin();
+            //SecurityFilterChain에 추가하기
+            httpSecurity.apply(new CustomSecurityConfigurer().setFlag(false)); //apply를 통하여 SecurityConfigurer를 추가한다.
+            return httpSecurity.build();
+
+    }
+}
