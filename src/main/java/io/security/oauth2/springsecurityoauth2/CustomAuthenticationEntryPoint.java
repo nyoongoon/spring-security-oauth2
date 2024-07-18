46f77ab1df0892352a53337fc2414503a3036727
nyoongoon <nyoongoon@naver.com>
2024-07-19 06:12:02 +0900

    BASIC 인증 정리

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // WWW-Authenticate 라는 규약을 지키지 않으면 브라우저는 Basic인증창을 띄우지 않음!
+        response.addHeader("WWW-Authenticate", "Basic realm=localhost");
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    }
+}
