ad81f2a8f196219ecf3a22cde681c5264e8e4f1a
nyoongoon <nyoongoon@naver.com>
2024-07-22 06:13:01 +0900

    시큐리티 CORS 이론 및 예시코드

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -13,9 +16,23 @@ public class SecurityConfig {
      */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-            httpSecurity.authorizeRequests().anyRequest().authenticated();
-            //SecurityFilterChain에 추가하기
-            httpSecurity.apply(new CustomSecurityConfigurer().setFlag(false)); //apply를 통하여 SecurityConfigurer를 추가한다.
-            return httpSecurity.build();
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+        //SecurityFilterChain에 추가하기
+//            httpSecurity.apply(new CustomSecurityConfigurer().setFlag(false)); //apply를 통하여 SecurityConfigurer를 추가한다.
+        httpSecurity.cors().configurationSource(corsConfigurationSource())
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
